b000a69b8572c83886a25d154fef6123b46eef8a
Andre Arko <andre@arko.net>
2018-10-18 23:29:23 -0700

    provide Unpwn.pwned?
    This will check the local bloom filter of the top million most common
    passwords, and if the offered password passes that check, it will then
    run it through the `pwned` gem's check against the haveibeenpwned.com
    API to make sure the password hasn't been used anywhere. If both of
    those checks pass, or haveibeenpwned is down, it will return `false`.

--- lib/unpwn.rb
+++ lib/unpwn.rb
@@ -1,6 +1,20 @@
 require "unpwn/version"
 
+require "bloomer/msgpackable"
+require "pwned"
+
+# Unpwn.pwned? tells you if a password should be rejected.
 module Unpwn
-  class Error < StandardError; end
-  # Your code goes here...
+
+  def self.pwned?(password)
+    bloom.include?(password) || Pwned.pwned?("password")
+  end
+
+  def self.bloom
+    @bloom ||= begin
+      top = File.read File.expand_path("top1000000.msgpack", __dir__)
+      Bloomer.from_msgpack(top)
+    end
+  end
+
 end
