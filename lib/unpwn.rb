ac97ba4ae97c5ad8f8c463d2a039ebb217cf0477
Andre Arko <andre@arko.net>
2019-01-08 01:31:01 -0800

    make Unpwn a class with min/max and #acceptable?

--- lib/unpwn.rb
+++ lib/unpwn.rb
@@ -4,13 +4,28 @@ require "bloomer/msgpackable"
 require "pwned"
 
 # Unpwn.pwned? tells you if a password should be rejected.
-module Unpwn
+class Unpwn
 
-  def self.pwned?(password)
-    bloom.include?(password) || Pwned.pwned?("password")
+  def initialize(min: 8, max: nil)
+    raise ArgumentError if min && min < 8
+    raise ArgumentError if max && max < 64
+
+    @min = min
+    @max = max
   end
 
-  def self.bloom
+  def acceptable?(password)
+    return false if @min && password.size < @min
+    return false if @max && password.size > @max
+
+    !pwned?(password)
+  end
+
+  def pwned?(password)
+    bloom.include?(password) || Pwned.pwned?(password)
+  end
+
+  def bloom
     @bloom ||= begin
       top = File.read File.expand_path("top1000000.msgpack", __dir__)
       Bloomer.from_msgpack(top)
--- lib/unpwn/version.rb
+++ lib/unpwn/version.rb
@@ -1,3 +1,3 @@
-module Unpwn
+class Unpwn
   VERSION = "0.1.0"
 end
